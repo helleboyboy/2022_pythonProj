d53177991a2b730360a2f29e4638ca03982f6747
java页 <1786076351@qq.com>
2022-09-20 22:34:24 +0800

    单挑http数据发送

---
+++ com/study/http_json.py
@@ -0,0 +1,66 @@
+# coding = utf-8
+import json
+import logging
+import urllib.parse
+import urllib.request
+from time import *
+
+logging.basicConfig(level=logging.DEBUG)
+# def post_json_by_http(json_value, url, headers):
+#     data = urllib.parse.urlencode(json_value).encode('utf-8')
+#     request = urllib.request.Request(url, data, headers)
+#     urllib.request.urlopen(request).read().decode('utf-8')
+
+def urllib2_send_post(send_url, data, headers=[]):
+    req = urllib.request.Request(send_url, data=data)
+    for header in headers:
+        req.add_header(header.split(':', 1)[0], header.split(':', 1)[1])
+    resJson = None
+    try:
+        response = urllib.request.urlopen(req)
+        res = response.read()
+        resJson = json.loads(eval(res))
+    except urllib.error.HTTPError as e:
+        logging.error(e.code)
+        logging.error(send_url)
+    except urllib.error.URLError as e:
+        logging.error(e.reason)
+        logging.error(send_url)
+    except Exception as e:
+        logging.error(str(e))
+        logging.error(send_url)
+    return resJson
+
+
+def getValue(cluster, service, component, component_state, host, ip):
+    my_dict = {'cluster': cluster,
+               'service': service,
+               'component': component,
+               'component_state': component_state,
+               'host': host,
+               'ip': ip,
+               'ts': '1234555566'
+               }
+    my_json = json.dumps(my_dict).encode()
+    return my_json
+
+
+collect_time = time()
+url = "http://192.168.52.19:4444/test_json_by_http"
+headers = ["Content-Type:application/json; charset=utf-8"]
+count = 0
+bounary = 100
+while count < bounary:
+    count = count + 1
+    cluster = 'cluster' + str(count)
+    service = 'service' + str(count)
+    component = 'component' + str(count)
+    component_state = 'componentState' + str(count)
+    host = 'leader-myhost' + str(count)
+    ip = '192.168.52.' + str(count)
+    result_json = getValue(cluster, service, component, component_state, host, ip)
+    # 单独发送！！！
+    urllib2_send_post(url, result_json, headers)
+# post_json_by_http(result_json_list, url, headers)
+
+print("=== end ===")
--- com/study/notice.py
+++ com/study/notice.py
@@ -1,9 +1,143 @@
 # -*- coding: UTF-8 -*-
+
+
 """
 # == 根据value来判断是否相等
 # is 根据对象的id来判断是否相等
 # is not 根据对象的id来判断是否不相等
 """
-print(1 == True)
-print(1 is True)
-print(1 is not True)
+
+
+def test_equal():
+    print(1 == True)
+    print(1 is True)
+    print(1 is not True)
+
+
+# test_equal()
+
+"""
+打印换行或者不换行
+利用print的参数end来控制结束符号！默认为\n
+"""
+
+
+def test_print_end():
+    print("aaa", end=",")
+    print("aaa", end="\t")
+    print("aaa", end="")
+    print("bbb")
+    print("ccc")
+
+
+# test_print_end()
+
+"""
+九九乘法表
+"""
+
+
+def nine_nine_mult(count):
+    # count = 9
+    i = 1
+    while i <= count:
+        j = 1
+        while j <= i:
+            print(f"{j} * {i} = {i * j}", end="\t")
+            j += 1
+        print()
+        i += 1
+
+
+# nine_nine_mult(9)
+
+"""
+continue
+break
+break,continue均对最近的一次循环有效！！！
+paas 用来占位！
+"""
+
+
+def test_break():
+    a = 0
+    while a < 10:
+        a += 1
+        b = 0
+        while b < 10:
+            b += 1
+            if b == 3:
+                break
+            print(a, b)
+        print("----一圈----")
+
+
+# test_break()
+
+
+def test_continue():
+    c = 0
+    while c < 10:
+        c += 1
+        d = 0
+        while d < 10:
+            d += 1
+            if d == 3:
+                continue
+            print(d, d)
+        print("----一圈----")
+
+
+# test_continue()
+
+
+def test_pass():
+    a = 0
+    while a <= 5:
+        pass
+        # print(a)
+        a += 1
+    print("循环结束！")
+
+
+# test_pass()
+
+"""
+测试切片
+切片得到的结果也是列表类型
+"""
+
+
+def test_qie_pian():
+    a = [1, 2, 3, 4]
+    result = a[1:4]
+    print(result, type(result))
+
+
+# test_qie_pian()
+
+
+def test_in(loc_num):
+    a = [1, 2, 3, 4, 5]
+    if loc_num in a:
+        print(loc_num)
+    else:
+        print("not in")
+
+
+# test_in(1)
+# test_in(10)
+
+
+"""
+:: 表示取列表的所有元素，可以通过步长来取元素
+"""
+
+
+def test_double_mao_hao(step):
+    a = [1, 2, 3, 4, 5]
+    print(a[::step])
+
+
+# test_double_mao_hao(1)
+# test_double_mao_hao(2)
